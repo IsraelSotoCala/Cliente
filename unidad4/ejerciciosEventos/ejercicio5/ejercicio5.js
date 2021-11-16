51a72078d0b51c4aa1fe579639276abfdad52e89
Israel Soto <91608660+IsraelSotoCala@users.noreply.github.com>
2021-11-16 14:54:06 +0100

    Add files via upload

---
+++ unidad4/ejerciciosEventos/ejercicio5/ejercicio5.js
@@ -0,0 +1,14 @@
+function sacarUrl1() {
+    let url = document.getElementById('bird').src;
+    alert(url)
+}
+
+function sacarUrl2() {
+    let url = document.getElementById('poyo').src;
+    alert(url)
+}
+
+function sacarUrl3() {
+    let url = document.getElementById('ganon').src;
+    alert(url)
+}
