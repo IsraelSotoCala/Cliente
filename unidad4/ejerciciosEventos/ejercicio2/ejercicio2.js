c6a84c11e446e11827dbc62f442db384c838f95f
Israel Soto <91608660+IsraelSotoCala@users.noreply.github.com>
2021-11-16 14:50:13 +0100

    Add files via upload

---
+++ unidad4/ejerciciosEventos/ejercicio2/ejercicio2.js
@@ -0,0 +1,11 @@
+function cambiarColor() {
+    document.getElementById('btn').style.backgroundColor = '#FFFF00';
+}
+
+function cambiarFondo() {
+    document.getElementById('key').style.backgroundColor = '#CCE6FF';
+}
+
+function volverFondo() {
+    document.getElementById('key').style.backgroundColor = '#FFF';
+}
